a6da8e8bc4ae7bd665d1af0bea4703013df0b0a7
Ali <mail@ali.af>
2021-01-04 04:09:33 -0800

    Update

--- main.swift
+++ main.swift
@@ -13,9 +13,14 @@ while(true) {
 
 // Remove files in /_public/
 while (true) {
-	let filesInDirectory = try! FileManager.default.contentsOfDirectory(at: URL(fileURLWithPath: rootDirectory + "/_public"), includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
-	for file in filesInDirectory {
-		try! FileManager.default.removeItem(at: file)
+	let filesInDirectory = try? FileManager.default.contentsOfDirectory(at: URL(fileURLWithPath: rootDirectory + "/_public"), includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+	for file in (filesInDirectory ?? []) {
+		do {
+			try? FileManager.default.removeItem(at: file)
+		}
+		catch {
+			print("The file \(file.path) could not be opened")
+		}
 	}
 	break
 }
